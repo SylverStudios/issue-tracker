47c8c64e3d49cb23906a951642e3f40eb3b496ca
Sam Roberts <samgqroberts@gmail.com>
2017-04-09 12:54:43 -0400

    set up react app and all the good configs

---
+++ karma.conf.js
@@ -0,0 +1,56 @@
+// Karma configuration
+// Generated on Mon Apr 04 2016 12:27:33 GMT-0400 (EDT)
+
+module.exports = function (config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: 'app/',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'chai'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      '**/*.test.js',
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      '**/*.test.js': ['webpack'],
+    },
+
+    webpack: Object.assign({}, require('./webpack.config.js'), { externals: [] }),
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'mocha', 'nyan' (nyan is the best one)
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['mocha'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // use Chrome as the testing environment
+    browsers: ['Chrome'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+  });
+};
